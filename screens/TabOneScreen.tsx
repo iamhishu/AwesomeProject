2ea6fabd1a0a08be494b8b78f0c7cea1416d6c51
iamhishu <hitesh3380@gmail.com>
2021-12-30 07:34:43 +0530

    native-base-boiler-plate

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,16 @@
 import { StyleSheet } from 'react-native';
-
+import {  Switch as RNSwitch } from "react-native";
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import { connect } from "react-redux";
+import { withFormikControl } from "react-native-formik";
 
 const TabOne = function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>User Sign Up/Login</Text>
+      <View style={styles.separator} lightColor="#e5e5e5" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
